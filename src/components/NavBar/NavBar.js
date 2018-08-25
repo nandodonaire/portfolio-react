3ae5ffb1994eb3884f59564dfce6a60951282d9c
nandodonaire <nandodonaire@gmail.com>
2018-08-25 17:57:21 -0400

    Added Header section with picture of volcano and job title

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,28 +2,28 @@ import React from 'react';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
 
 const NavBar = () => (
-    <Navbar inverse collapseOnSelect>
+    <Navbar inverse collapseOnSelect fixedTop>
     <Navbar.Header>
         <Navbar.Brand>
-        <a href="#home">Fernando Donaire</a>
+        <a href="/">Fernando Donaire</a>
         </Navbar.Brand>
         <Navbar.Toggle />
     </Navbar.Header>
     <Navbar.Collapse>
         <Nav pullRight>
-        <NavItem eventKey={1} href="#">
+        <NavItem eventKey={1} href="#about-me">
             About Me
         </NavItem>
-        <NavItem eventKey={2} href="#">
+        <NavItem eventKey={2} href="#skills">
             Skills
         </NavItem>
-        <NavItem eventKey={3} href="#">
+        <NavItem eventKey={3} href="#resume">
             Resume
         </NavItem>
-        <NavItem eventKey={4} href="#">
+        <NavItem eventKey={4} href="#projects">
             Projects
         </NavItem>
-        <NavItem eventKey={5} href="#">
+        <NavItem eventKey={5} href="#contact-me">
             Contact Me
         </NavItem>
         </Nav>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from '../components/NavBar/NavBar'
+import Header from '../components/Header/Header'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
+        <Header />
       </div>
     );
   }
